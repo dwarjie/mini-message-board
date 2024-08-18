1b32b83036983b396a713c0237111376ae2bf113
dwarjie <macoycoder@gmail.com>
2024-08-18 15:01:34 +0800

    Added views and posting of new message

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -2,7 +2,19 @@ const asyncHandler = require("express-async-handler");
 const messageModel = require("../model/messages.model");
 
 const getMessages = asyncHandler(async (req, res) => {
-	res.send(messageModel.messages);
+	res.render("index", {
+		title: "Mini-message-board",
+		messages: messageModel.messages,
+	});
 });
 
-module.exports = { getMessages };
+const createMessage = asyncHandler(async (req, res) => {
+	messageModel.messages.push({
+		text: req.body.message,
+		user: req.body.user,
+		added: new Date(),
+	});
+	res.redirect("back");
+});
+
+module.exports = { getMessages, createMessage };
--- routes/message.routes.js
+++ routes/message.routes.js
@@ -3,5 +3,6 @@ const messageController = require("../controllers/message.controller");
 const router = Router();
 
 router.get("/", messageController.getMessages);
+router.post("/new", messageController.createMessage);
 
 module.exports = router;
